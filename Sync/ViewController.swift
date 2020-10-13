6b1014d94f262969efe1d8b3ace55c9a5f2349ab
Gabriela Pulido <jabyyp@gmail.com>
2020-10-13 10:42:50 -0500

    Initial Commit

---
+++ Sync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sync
+//
+//  Created by Gabriela Pulido on 10/13/20.
+//  Copyright © 2020 Gabriela Pulido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
