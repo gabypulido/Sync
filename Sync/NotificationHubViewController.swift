25b0388c15212a97bd7275b9cb395c7294d125d4
Gabriela Pulido <jabyyp@gmail.com>
2020-10-20 10:38:33 -0500

    Modify hub view

--- Sync/NotificationHubViewController.swift
+++ Sync/NotificationHubViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class NotificationHubViewController: UIViewController {
+    
+    var socialMedia: [String] = ["Twitter", "Messenger", "LinkedIn", "Facebook", ]
     @IBOutlet weak var notificationHubTable: UITableView!
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //tableCell.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
         // Do any additional setup after loading the view.
+        notificationHubTable.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
     }
     
-     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return socialMedia.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+        let row = indexPath.row
+        let socialMediaNetwork = socialMedia[row]
+
+        return cell
+    }
     
 
     /*
