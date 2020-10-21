8947bef534bfed12aac1d5e63eba7e4810cb9f6c
Gabriela Pulido <jabyyp@gmail.com>
2020-10-21 14:54:22 -0500

    add headers for each social media

--- Sync/NotificationHubViewController.swift
+++ Sync/NotificationHubViewController.swift
@@ -8,6 +8,16 @@
 
 import UIKit
 
+struct Category {
+   let name : String
+   var items : [Notification]
+}
+
+struct Notification {
+    var body = String()
+    var time = String()
+}
+
 class SocialMediaTableViewCell: UITableViewCell {
     @IBOutlet weak var socialMediaNameLabel: UILabel!
     
@@ -16,6 +26,13 @@ class SocialMediaTableViewCell: UITableViewCell {
 class NotificationHubViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var socialMedia: [String] = ["Twitter", "Messenger", "LinkedIn", "Facebook", ]
+    var twitterNotifications = [Notification(body: "Twitter Test", time: "time")]
+    var messengerNotifications = [Notification(body: "Messenger Test", time: "time")]
+    var linkedInNotifications = [Notification(body: "LinkedIn Test", time: "time")]
+    var facebookNotifications = [Notification(body: "Facebook Test", time: "time")]
+    
+    var sections = [Category]()
+    
     @IBOutlet weak var notificationHubTable: UITableView!{
         didSet {
                 notificationHubTable.dataSource = self
@@ -29,27 +46,33 @@ class NotificationHubViewController: UIViewController, UITableViewDelegate, UITa
         super.viewDidLoad()
         //tableCell.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
         // Do any additional setup after loading the view.
+        sections = [Category(name: "Twitter", items: twitterNotifications), Category(name: "Messenger", items: messengerNotifications), Category(name: "LinkedIn", items: linkedInNotifications), Category(name: "Facebook", items: facebookNotifications)]
         notificationHubTable.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
-    }
+        }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return socialMedia.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! SocialMediaTableViewCell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! SocialMediaTableViewCell
+//        cell.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
+//        let row = indexPath.row
+//        let socialMediaNetwork = socialMedia[row]
+//        cell.socialMediaNameLabel.text = socialMediaNetwork
+//
+//        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell") as! SocialMediaTableViewCell
+        let items = self.sections[indexPath.section].items
+        //let item = items[indexPath.row]
         cell.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
-        let row = indexPath.row
-        let socialMediaNetwork = socialMedia[row]
-        cell.socialMediaNameLabel.text = socialMediaNetwork
-
         return cell
     }
     
      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         tableView.deselectRow(at: indexPath, animated: true)
-        switch socialMedia[indexPath.row] {
-            case "Twitter":
+        switch sections[indexPath.section].name {
+        case "Twitter":
                 self.performSegue(withIdentifier: "TwitterChannelSegue", sender: self)
         case "Messenger":
             self.performSegue(withIdentifier: "MessengerChannelSegue", sender: self)
@@ -61,6 +84,22 @@ class NotificationHubViewController: UIViewController, UITableViewDelegate, UITa
                 print("default")
         }
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sections[section].name
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor(hue: 0.6167, saturation: 0.17, brightness: 0.44, alpha: 1.0)
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = UIColor(hue: 0.125, saturation: 0.11, brightness: 0.98, alpha: 1.0)
+    }
+
+
     
     
 
