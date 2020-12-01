8fa57f4f05114bcd72db698be97e2cac8987818f
jyse0 <jyseo@utexas.edu>
2020-12-01 05:58:42 -0600

    corrected minor style issues and code cleanup

--- Sync/TwitterChannelViewController.swift
+++ Sync/TwitterChannelViewController.swift
@@ -38,8 +38,6 @@ class TwitterChannelViewController: UIViewController, UITableViewDelegate, UITab
         
         let mainVC = NotificationHubViewController()
         self.fullNotifications = mainVC.retrieveNotifications()
-        print("count  \(fullNotifications.count)")
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,13 +47,8 @@ class TwitterChannelViewController: UIViewController, UITableViewDelegate, UITab
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! TwitterTableViewCell
         cell.backgroundColor = UIColor(hue: 0.5222, saturation: 0.22, brightness: 0.87, alpha: 1.0)
-        
         let row = indexPath.row
-        
-        print("current row is: \(row)")
-//        let currNotification = fullNotifications[row]
         let currNotification = filterNotifications()[row]
-        
         
         cell.socialIcon.image = UIImage(named: "twitter-64")
         cell.notificationBody.text = currNotification.value(forKey: "notificationContent") as? String
@@ -69,31 +62,26 @@ class TwitterChannelViewController: UIViewController, UITableViewDelegate, UITab
         dropDown.dataSource = ["All", "Retweet", "Mention", "Direct Message"]
         dropDown.anchorView = (sender as! AnchorView)
         dropDown.bottomOffset = CGPoint(x: 0, y: (sender as AnyObject).frame.size.height)
-            dropDown.show()
-            dropDown.selectionAction = { [weak self] (index: Int, item: String) in
-                guard let _ = self else { return }
-                (sender as AnyObject).setTitle(item, for: .normal)
-                self!.filterType = item
-                self!.twitterNotificationTable.reloadData()
-            }
+        dropDown.show()
+        dropDown.selectionAction = { [weak self] (index: Int, item: String) in
+            guard let _ = self else { return }
+            (sender as AnyObject).setTitle(item, for: .normal)
+            self!.filterType = item
+            self!.twitterNotificationTable.reloadData()
+        }
     }
     
     func filterNotifications() -> [NSManagedObject] {
-            switch self.filterType {
-                case "Retweet":
-                    print("Retweet")
-                    
-                    return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Retweet") }
-                case "Mention":
-                    print("Mention")
-                    return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Mention") }
-                case "Direct Message":
-                    print("Direct message does not work")
-                    return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Direct Message") }
-                default:
-                    print("All or default")
-                    return fullNotifications
-            }
+        switch self.filterType {
+            case "Retweet":
+                return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Retweet") }
+            case "Mention":
+                return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Mention") }
+            case "Direct Message":
+                return fullNotifications.filter{ $0.value(forKey: "notificationType") as? String == ("Direct Message") }
+            default:
+                return fullNotifications
         }
+    }
 
 }
